c706bf7f78505d6bd68a18b512730ba49957fc0f
Neko Collector <nekocollector@terminal.com>
2026-03-31 02:13:51 +0800

    :3

--- content/v_moveset2.lua
+++ content/v_moveset2.lua
@@ -9442,8 +9442,8 @@ AddModule(function()
 		
 		-- sword
 		sword.Offset = joints.sw
-			* CFrame.new(-0.1, -1.63, -0.89, -1, 0, 0, 0, 0, -1, 0, -1, 0)
-			* CFrame.new(0, -3.94, 2.94, 1, 0, 0, 0, 0, 1, 0, -1, 0)
+			* CFrame.new(-0.1, -1.63, -0.89, -1, 0, 0, 0, 0, -1, 0, -1, 0):Inverse()
+			* CFrame.new(0, -3.94, 2.94, 1, 0, 0, 0, 0, 1, 0, -1, 0):Inverse()
 			* CFrame.Angles(1.57, 0, 0)
 		sword.Disable = not not isdancing
 	end
